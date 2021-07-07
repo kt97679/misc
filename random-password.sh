8aec23de637dec3b2b4e8369688b319262b75168
Kirill Timofeev <kt97679@gmail.com>
2021-07-06 21:00:09 -0700

    using 'sort -R' instead of /dev/urandom

--- random-password.sh
+++ random-password.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 password_length=${1:-16}
+special_symbols="~,./@#%&?=+_-"
 
-tr -dc [:graph:] </dev/urandom \
-    | fold -w $password_length \
+while true; do
+    echo {a..z} {A..Z} {0..9} \
+        | grep -oP "\S" \
+        | sort -R \
+        | head -n$((password_length - 1)) \
+        | ( cat && echo ${special_symbols:$((RANDOM % ${#special_symbols})):1} ) \
+        | sort -R \
+        | tr -d '\n'
+    echo
+done \
     | sed '/[a-z]/!d; /[A-Z]/!d; /[0-9]/!d' \
-    | grep "^[a-zA-Z0-9]*[~,./@#%&?=+_-][a-zA-Z0-9]*$" \
-    | grep -v "\(.\).*\1" \
     | head -n20
