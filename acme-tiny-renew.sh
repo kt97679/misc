3def3b49cd2347c9fc590811552242ef9b0f2072
Kirill Timofeev <kt97679@gmail.com>
2021-11-15 00:30:35 +0000

    acme_dir creation added

--- acme-tiny-renew.sh
+++ acme-tiny-renew.sh
@@ -8,6 +8,7 @@ set -uex
 
 exec &> >(tee /tmp/$(basename $0).log)
 
+acme_dir="/var/www/challenges/"
 seconds_per_day=$((60 * 60 * 24))
 base_dir="/etc/acme-tiny"
 account_key="${base_dir}/account.key"
@@ -34,7 +35,8 @@ renew_domain() {
     openssl genrsa 4096 > "$new_privkey_pem"
     # Create a certificate signing request (CSR) for your domain
     openssl req -new -sha256 -key "$new_privkey_pem" -subj "/CN=${domain}" > "$new_domain_csr"
-    acme-tiny --account-key "$account_key" --csr "$new_domain_csr" --acme-dir /var/www/challenges/ > "$new_fullchain_pem"
+    mkdir -p "$acme_dir"
+    acme-tiny --account-key "$account_key" --csr "$new_domain_csr" --acme-dir "$acme_dir" > "$new_fullchain_pem"
     ln -nsf $new_domain_dir $domain_dir
     ls -dt ${domain_dir}.* | tail -n +2 | xargs rm -rf
     certificate_updated=true
