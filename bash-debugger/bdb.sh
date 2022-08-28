214bbb8c132b11d6d1a759fbfc758e261559602c
delameter <0.delameter@gmail.com>
2022-08-28 20:56:43 +0300

    - add [number] of triggered breakpoint to output

--- bash-debugger/bdb.sh
+++ bash-debugger/bdb.sh
@@ -32,13 +32,15 @@ __dbg__trap() {
     set +eu
     ((__dbg__trace == 1)) \
         && echo "$(_e 36)${BASH_SOURCE[1]}:$(_e 32)${BASH_LINENO[0]}:$(_e) $BASH_COMMAND"
-    for __dbg__breakpoint in "${__dbg__breakpoints[@]}"; do
-        eval "$__dbg__breakpoint" && __dbg__do_break=true && break
+
+    for __dbg__breakpoint_num in $(seq ${#__dbg__breakpoints[@]}); do
+        __dbg__breakpoint_idx=$((__dbg__breakpoint_num - 1))
+        eval "${__dbg__breakpoints[__dbg__breakpoint_idx]}" && __dbg__do_break=true && break
     done
 
     ((__dbg__trace == 2)) || $__dbg__do_break && {
         ((__dbg__trace == 0)) \
-            && echo -n "$(_e 36)$(basename "${BASH_SOURCE[1]}"):" \
+            && echo -n "$(_e 31)[$__dbg__breakpoint_num] $(_e 36)$(basename "${BASH_SOURCE[1]}"):" \
             && echo "$(_e 32)${BASH_LINENO[0]}: $(_e) $BASH_COMMAND"
 
         ((__dbg__trace == 2)) && __dbg__trace=0
