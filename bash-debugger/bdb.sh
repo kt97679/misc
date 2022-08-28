0b92e31b916d9909915671ff7bfdfc0c5cc4ac11
delameter <0.delameter@gmail.com>
2022-08-28 19:58:12 +0300

    - debugger output colorizing

--- bash-debugger/bdb.sh
+++ bash-debugger/bdb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# shellcheck disable=SC2162,SC1090
+
+_e() { echo $'\e['"${1:-}"m ; }
 
 __dbg__breakpoints=()
 __dbg__trace=2
@@ -7,15 +10,18 @@ __dbg__trap() {
         __dbg__do_break=false
     set +eu
     ((__dbg__trace == 1)) \
-        && echo "+(${BASH_SOURCE[1]}:${BASH_LINENO[0]}): $BASH_COMMAND"
+        && echo "$(_e 36)${BASH_SOURCE[1]}:$(_e 32)${BASH_LINENO[0]}:$(_e) $BASH_COMMAND"
     for __dbg__breakpoint in "${__dbg__breakpoints[@]}"; do
         eval "$__dbg__breakpoint" && __dbg__do_break=true && break
     done
+
     ((__dbg__trace == 2)) || $__dbg__do_break && {
         ((__dbg__trace == 0)) \
-            && echo "+(${BASH_SOURCE[1]}:${BASH_LINENO[0]}): $BASH_COMMAND"
+            && echo -n "$(_e 36)$(basename "${BASH_SOURCE[1]}"):" \
+            && echo "$(_e 32)${BASH_LINENO[0]}: $(_e) $BASH_COMMAND"
+
         ((__dbg__trace == 2)) && __dbg__trace=0
-        while read -p "bdb> "  __dbg__cmd __dbg__cmd_args; do
+        while read -p "$(_e 34)bdb> $(_e)"  __dbg__cmd __dbg__cmd_args; do
             case $__dbg__cmd in
                 '') eval "$__dbg__set" && return 0 ;;
                 trace) ((__dbg__trace ^= 1)) ;;
