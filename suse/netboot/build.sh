c910f0c1a2ab099ea26d50dfba82bf8ff6d4e828
Kirill Timofeev <kt97679@gmail.com>
2023-10-29 17:00:23 -0400

    opensuse netboot image build added

---
+++ suse/netboot/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -xeu
+
+exec &> >(tee /tmp/$(basename $0).log)
+
+IMAGE_ROOT="/new_root"
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+
+[ -z "${PACKER_BUILD_NAME:-}" ] && {
+    exec packer build -var IMAGE="registry.opensuse.org/opensuse/leap:latest" build.json
+}
+
+at_exit() {
+    chown -R --reference=$0 $SCRIPT_DIR
+}
+
+trap at_exit EXIT
+
+mkdir -p /build/output "$IMAGE_ROOT"
+
+zypper update -y
+zypper install -y squashfs
+rpm --root $IMAGE_ROOT --import /usr/lib/rpm/gnupg/keys/*.asc
+zypper --root $IMAGE_ROOT ar --refresh http://download.opensuse.org/distribution/leap/15.6/repo/oss/ oss
+zypper --root $IMAGE_ROOT in -y --no-recommends kernel-default zypper bash util-linux openssh-server dracut squashfs curl dhcp-client device-mapper tar NetworkManager
+
+pseudo_fs=(
+    "dev     devtmpfs                   devtmpfs"
+    "dev/pts devpts   -o gid=5,mode=620 devpts"
+    "proc    proc                       proc"
+    "run     tmpfs    -o mode=755       tmpfs"
+    "sys     sysfs                      sysfs"
+    "tmp     tmpfs                      tmpfs"
+    "var/tmp tmpfs                      tmpfs"
+)
+
+clear_mount_point() {
+    find ${IMAGE_ROOT}/$1 -mindepth 1 -print0 | xargs -0 --no-run-if-empty rm -rf
+}
+
+printf "%s\n" "${pseudo_fs[@]}"|while read mount_point mount_options; do
+    clear_mount_point $mount_point
+    mount -t $mount_options ${IMAGE_ROOT}/$mount_point
+done
+chmod 1777 "${IMAGE_ROOT}/dev/shm"
+
+chroot ${IMAGE_ROOT} /bin/bash -x <<CHROOT
+sed -i -e '/^PermitRootLogin/d' /etc/ssh/sshd_config
+echo -e "\nPermitRootLogin without-password" >>/etc/ssh/sshd_config
+rm -rf /etc/{hostname,hosts} /var/log/*.log /root/.cache
+> /etc/machine-id
+mkdir -p /root/.ssh && ssh-keygen -N '' -f /root/.ssh/ssh-key
+mv /root/.ssh/ssh-key.pub /root/.ssh/authorized_keys
+dracut -f --regenerate-all -m "livenet" --add-drivers "dm_mod virtio virtio_pci virtio_blk virtio_net loop squashfs" --install "/usr/bin/stat"
+CHROOT
+
+printf "%s\n" "${pseudo_fs[@]}"|tac|while read mount_point mount_options; do
+    umount ${IMAGE_ROOT}/${mount_point}
+    clear_mount_point $mount_point
+done
+
+mv ${IMAGE_ROOT}/root/.ssh/ssh-key /build/output/
+cp ${IMAGE_ROOT}/boot/{vmlinuz,initrd}*default /build/output/
+rm -f /build/output/root.squashfs
+mksquashfs ${IMAGE_ROOT} /build/output/root.squashfs -b 1048576 -comp xz -Xdict-size 100% -regex -e "proc/.*" -e "sys/.*" -e "run/.*" -e "var/cache/.*" -e "boot/.*"
---
+++ suse/netboot/qemu.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -ue
+
+output_dir="$(dirname $(readlink -f $0))/output"
+wait_seconds=120
+ssh_config=$output_dir/ssh_config
+
+run_ssh() {
+    ssh -F $ssh_config qemu "$@"
+}
+
+get_free_port() {
+    local port
+    while true; do
+        port=$((1024 + RANDOM % 8192))
+        netstat -lnt|grep -q ":${port}\b" && continue
+        echo $port
+        return
+    done
+}
+
+kill_pids() {
+    local pids=$(cat $output_dir/*.pid 2>/dev/null)
+    [ -n "$pids" ] && kill ${1:-} $pids >/dev/null 2>&1
+}
+
+start() {
+    local console=false cmd_suffix=">qemu.log 2>&1 &"
+    kill_pids -0 && echo "Already running" && exit
+    [ "${1:-}" == "console" ] && console=true
+    cd $output_dir
+    http_port=$(get_free_port)
+    web_root="http://10.0.2.2:$http_port"
+    for x in *; do
+        url=$web_root/$x
+        case $x in
+            initrd*) initrd=$url ;;
+            vmlinuz*) vmlinuz=$url ;;
+            *.squashfs) squashfs=$url ;;
+        esac
+    done
+    printf "%s\n" \
+        "#!ipxe" \
+	"kernel $vmlinuz ip=dhcp rootfstype=squashfs rd.live.squashimg=$(basename $squashfs) root=live:$squashfs nomodeset console=ttyS0,115200n8 rootsize=10%" \
+        "initrd $initrd" \
+        "boot" >boot.ipxe
+    python3 -m http.server $http_port >http.log 2>&1 &
+    echo $! >http.pid
+    domainname="$(hostname -d)"
+    [ -z "$domainname" ] && domainname="unknown"
+    ssh_port=$(get_free_port)
+    printf "%s\n" \
+        "Host qemu" \
+        "  HostName 127.0.0.1" \
+        "  User root" \
+        "  Port $ssh_port" \
+        "  IdentityFile $output_dir/ssh-key" \
+        "  UserKnownHostsFile /dev/null" \
+        "  StrictHostKeyChecking no" \
+        "  PasswordAuthentication no" \
+        "  LogLevel FATAL" > $ssh_config
+    $console && cmd_suffix="2>&1|tee qemu.log"
+    eval qemu-system-x86_64 ${QEMU_OPTS:-} \
+        -boot n \
+        -device virtio-net-pci,netdev=n1 \
+        -netdev user,id=n1,tftp=${output_dir},bootfile=/boot.ipxe,hostfwd=tcp::${ssh_port}-:22,domainname=${domainname} \
+        -nographic \
+        -m 4096 "$cmd_suffix"
+#        -enable-kvm \
+#        -cpu max >qemu.log 2>&1 &
+    echo $! >qemu.pid
+    deadline=$((SECONDS + wait_seconds))
+    while ((SECONDS < deadline)); do
+        run_ssh true >/dev/null 2>&1 && echo "VM is ready" && exit
+        sleep 5
+    done
+    echo "VM failed to start"
+}
+
+case ${1:-} in
+    start) start ;;
+    stop) kill_pids || true ;;
+    ssh) shift && run_ssh "$@" ;;
+    console) start console ;;
+    *) echo "Usage: $0 start|stop|ssh|console" ;;
+esac
