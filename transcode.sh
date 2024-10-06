0d56ef893807a6cabc09c64be4cf82fd438a33d8
Kirill Timofeev <kt97679@gmail.com>
2024-10-06 18:08:45 -0400

    transcode script added

---
+++ transcode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+FFMPEG_HOME=$HOME/Downloads/ffmpeg-git-20240629-amd64-static
+CRF=20
+
+src="$@"
+fps=$($FFMPEG_HOME/ffprobe "$src" |& grep -oP "[\d.]+(?=\sfps)")
+echo nice $FFMPEG_HOME/ffmpeg -i \""$src"\" -r $fps -acodec libmp3lame -vcodec libx265 -map 0 -map -v -map V -crf $CRF \"/var/tmp/$(basename "$src" | sed -e 's/[.][^.]*$//')\".mkv
