8b95bf97b2c5e34b4784b83126b765be40efa65c
Kirill Timofeev <kt97679@gmail.com>
2026-02-20 13:08:00 -0800

    only transcode 1st video stream, other video streams are usually cover arts

--- transcode2.sh
+++ transcode2.sh
@@ -6,4 +6,6 @@ FFMPEG_HOME=$HOME/Downloads/ffmpeg-master-latest-linux64-gpl/bin/
 CRF=20
 
 src="$@"
-echo nice $FFMPEG_HOME/ffmpeg -i \""$src"\" -map 0 -c copy -c:v libx265 -crf $CRF -preset slow -c:a libopus -b:a 96k -dn /var/tmp/\"$(basename "$src" | sed -e 's/[.][^.]*$//')\".mkv
+cat <<EOF
+nice $FFMPEG_HOME/ffmpeg -i "$src" -map 0:v:0 -map 0:a -map 0:s? -c:v libx265 -crf 20 -preset slow -c:a libopus -ac 2 -b:a 96k -c:s copy -dn /var/tmp/"$(basename "$src" | sed -e 's/[.][^.]*$//')".mkv
+EOF
